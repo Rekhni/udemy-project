2d9b1ab85697474e3bfa6dbaabf7f33ed4c73da5
Rakhymzhan <mark.john.99@mail.ru>
2023-07-31 23:33:12 +0600

    objects in function

--- js/script.js
+++ js/script.js
@@ -1,25 +1,111 @@
-function first() {
+// const personalPlanPeter = {
+//   name: "Peter",
+//   age: "29",
+//   skills: {
+//       languages: ['ru', 'eng'],
+//       programmingLangs: {
+//           js: '20%',
+//           php: '10%'
+//       },
+//       exp: '1 month'
+//   }
+// };
 
-  setTimeout(() => {
-    console.log(1);
-  }, 2000);
+// function showExperience(obj) {
+//   return obj.skills.exp;
+// }
+
+// const experience = showExperience(personalPlanPeter);
+// console.log(experience);
+
+
+// function showProgrammingLangs(plan) {
+//   const programmingLangs = plan.skills.programmingLangs;
+//   let result = '';
+
+//   for (let lang in programmingLangs) {
+//     result += `Язык ${lang} изучен на ${programmingLangs[lang]}\n`;
+//   }
+
+//   return result;
+// }
+
+// console.log(showProgrammingLangs(personalPlanPeter));
+
+
+
+const personalPlanPeter = {
+  name: "Peter",
+  age: "29",
+  skills: {
+      languages: ['ru', 'eng'],
+      programmingLangs: {
+          js: '20%',
+          php: '10%'
+      },
+      exp: '1 month'
+  },
+  showAgeAndLangs: function() {
+      return `Мне ${personalPlanPeter.age} и я владею языками: ${UpperCase()}`
+  }
 };
 
-function second() {
-  console.log(2);
+function UpperCase() {
+  let newArr = [];
+  const langs = personalPlanPeter.skills.languages;
+  for (let lang of langs) {
+    newArr.push(lang.toUpperCase());
+  }
+  return newArr.join(', ');
 }
 
-first();
-second();
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
 
 
-function learnJS(lang, callback) {
-  console.log(`Я учу: ${lang}`);
-  callback();
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+
+function showFamily(arr) {
+    return `Семья состоит из: ${arr.join(' ')}`;
 }
 
-function done() {
-  console.log('Я закончил курс!');
+console.log(showFamily(family)); 
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+  const standardizedArray = [];
+    for (let el of arr) {
+        standardizedArray.push(el.toLowerCase());
+    }
+    return standardizedArray;
+};
+
+const standardizedCities = standardizeStrings(favoriteCities);
+
+for (let city of standardizedCities) {
+  console.log(city);
 }
 
-learnJS('JavaScript', done);
+
+const someString = 'This is some strange string';
+
+function reverse(str) {
+    const newArr = [];
+    const reversedSentence = str.split(' ').reverse();
+    
+    for (let el of reversedSentence) {
+      newArr.push(el.split('').reverse().join(''));
+    }
+
+    return newArr.join(' ');
+}
+
+console.log(reverse(someString));
+
+
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+ 
+
+console.log(availableCurr(baseCurrencies, 'USD'));
