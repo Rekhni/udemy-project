483855ef5ce360c6514edf90892db53f1e442681
Rakhymzhan <mark.john.99@mail.ru>
2023-08-05 21:39:07 +0600

    навигация по DOM

--- js/script.js
+++ js/script.js
@@ -1,47 +1,67 @@
-const box = document.getElementById('box'),
-      btns = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+// const box = document.getElementById('box'),
+//       btns = document.getElementsByTagName('button'),
+//       circles = document.getElementsByClassName('circle'),
+//       wrapper = document.querySelector('.wrapper'),
+//       hearts = wrapper.querySelectorAll('.heart'),
+//       oneHeart = wrapper.querySelector('.heart');
 
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
+// // box.style.backgroundColor = 'blue';
+// // box.style.width = '500px';
 
-box.style.cssText = 'background-color: blue; width: 500px';
+// box.style.cssText = 'background-color: blue; width: 500px';
 
-btns[1].style.borderRadius = '100%';
+// btns[1].style.borderRadius = '100%';
 
-// for (let i = 0; i < hearts.length; i++) {
-//     hearts[i].style.backgroundColor = 'blue';
-// }
+// // for (let i = 0; i < hearts.length; i++) {
+// //     hearts[i].style.backgroundColor = 'blue';
+// // }
 
-hearts.forEach(item => {
-    item.style.backgroundColor = 'blue';
-});
+// hearts.forEach(item => {
+//     item.style.backgroundColor = 'blue';
+// });
 
-const div = document.createElement('div');
+// const div = document.createElement('div');
 
-div.classList.add('black');
+// div.classList.add('black');
 
-// wrapper.prepend(div);
-wrapper.append(div);
+// // wrapper.prepend(div);
+// wrapper.append(div);
 
-// hearts[0].before(div);
-// hearts[0].after(div);
+// // hearts[0].before(div);
+// // hearts[0].after(div);
 
-// circles[0].remove();
+// // circles[0].remove();
 
-// wrapper.insertBefore(div, hearts[2]);
+// // wrapper.insertBefore(div, hearts[2]);
 
-// wrapper.removeChild(hearts[1]);
+// // wrapper.removeChild(hearts[1]);
 
-// hearts[0].replaceWith(circles[0]); 
-// wrapper.replaceChild(circles[0], hearts[0])
+// // hearts[0].replaceWith(circles[0]); 
+// // wrapper.replaceChild(circles[0], hearts[0])
 
-div.innerHTML = '<h1>Hello World!</h1>';
+// div.innerHTML = '<h1>Hello World!</h1>';
 
-// div.textContent = 'Hello'; 
+// // div.textContent = 'Hello'; 
 
-div.insertAdjacentHTML("beforebegin", "<h2>Hello</h2>");
+// div.insertAdjacentHTML("beforebegin", "<h2>Hello</h2>");
+
+
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastElementChild);
+
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement.parentElement);
+
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text' || node.nodeName == '#comment') {
+        continue;
+    }
+
+    console.log(node);
+}
